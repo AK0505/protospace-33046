cea402d29ea4b3901c2134b03a86c05e332882c7
AK0505 <gjajtwmdmwt.59@gmail.com>
2020-12-29 15:15:55 +0900

    ActiveStorageの追加とバリデーションの追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,4 +2,9 @@ class Prototype < ApplicationRecord
   belongs_to :user
   has_many :comments
 
+    validates :title, presence: true
+    validates :cacth_copy, presence: true
+    validates :concept, presence: true
+    validates :image, presence: true
+
 end
