431505fc9dfa3a6c0da54cd23affd021160b5855
AK0505 <gjajtwmdmwt.59@gmail.com>
2021-01-04 11:36:44 +0900

    訂正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,53 +1,46 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
-
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
   end
-  
-  def new 
+
+  def new
     @prototype = Prototype.new
   end
 
-  
-  def show 
-    @prototype = Prototype.find(params[:id])
-    @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
-  end
-  
-  def edit 
-    @prototype = Prototype.find(params[:id])
-    unless user_signed_in? && current_user.id == @prototype.user.id
-      redirect_to action: :index
-    end
-  end
-  
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
-        redirect_to root_path
+      redirect_to root_path
     else
-        render :new
+      render :new
     end
-    
   end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+  end
+
   def update
-    prototype = Prototype.find(params[:id])
-    if  prototype.update(prototype_params)
-        redirect_to prototype_path(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
     else
-        render :edit
+      render :edit
     end
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
-    if  prototype.destroy
-        redirect_to root_path
+    if @prototype.destroy
+      redirect_to root_path
     else
-        render :edit
+      redirect_to root_path
     end
   end
 
@@ -57,4 +50,11 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
 end
